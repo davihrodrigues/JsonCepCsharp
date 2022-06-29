7b9a9cf19da7712c5bf851fd5ac3aa6a61d27eee
davihrodrigues <davi.haubricht@gmail.com>
2022-06-29 18:57:34 -0300

    Adicionar arquivos de projeto.

---
+++ Jsonrefit/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Refit;
+
+namespace ExemploRefit
+{
+    class MainClass
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var cepClient = RestService.For<ICepApiService>("https://viacep.com.br/");
+                Console.WriteLine("Informe seu cep:");
+
+                string cepInformado = Console.ReadLine().ToString();
+                Console.WriteLine("Consultando informacoes do CEP:" + cepInformado);
+
+                var address = await cepClient.GetAdressAsync(cepInformado);
+
+                Console.Write($"\nLogradouro:{address.Logradouro}\nBairro:{address.Bairro}\nCidade:{address.Localidade}");
+                Console.ReadKey();
+
+            }   catch (Exception e)
+            {
+                Console.WriteLine("Erro na consulta de cep: " + e.Message);
+            }
+        }   
+    }
+}
---
+++ Jsonrefit/ICepApiService.cs
@@ -0,0 +1,12 @@
+﻿using Refit;
+using System.Threading.Tasks;
+
+namespace ExemploRefit
+{
+    public interface ICepApiService
+
+    {
+        [Get("/ws/{cep}/json")]
+        Task<CepResponse> GetAdressAsync(string cep);
+    }
+}
